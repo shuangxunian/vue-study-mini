200ba9ffc23b8763edd3e75d22cf7076f5f48f55
yuelin.wang <yuelin.wang@nsdigital.io>
2026-03-06 15:02:44 +0800

    feat: effect

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -21,3 +21,36 @@ export function trigger(
 ) {
 	console.log('trigger: 触发依赖')
 }
+
+/**
+ * 单例的，当前的 effect
+ */
+export let activeEffect: ReactiveEffect | undefined
+
+/**
+ * 响应性触发依赖时的执行类
+ */
+export class ReactiveEffect<T = any> {
+	constructor(public fn: () => T) {}
+
+	run() {
+		// 为 activeEffect 赋值
+		activeEffect = this
+
+		// 执行 fn 函数
+		return this.fn()
+	}
+
+}
+
+/**
+ * effect 函数
+ * @param fn 执行方法
+ * @returns 以 ReactiveEffect 实例为 this 的执行函数
+ */
+export function effect<T = any>(fn: () => T) {
+	// 生成 ReactiveEffect 实例
+	const _effect = new ReactiveEffect(fn)
+	// 执行 run 函数
+	_effect.run()
+}
